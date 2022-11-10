f6687cbc25f05c691d796a66a12beed09a31903f
banku27 <pankajparihar123321@gmail.com>
2022-11-10 23:08:32 +0530

    log out

--- lib/features/home/drawers/profile_drawer.dart
+++ lib/features/home/drawers/profile_drawer.dart
@@ -6,6 +6,10 @@ import 'package:reddit_clone/theme/pallete.dart';
 class ProfileDrawer extends ConsumerWidget {
   const ProfileDrawer({super.key});
 
+  void logOut(WidgetRef ref) {
+    ref.read(authControllerProvider.notifier).logOut();
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider)!;
@@ -44,7 +48,7 @@ class ProfileDrawer extends ConsumerWidget {
                 Icons.logout,
                 color: Pallete.redColor,
               ),
-              onTap: () {},
+              onTap: () => logOut(ref),
             ),
             Switch.adaptive(
               value: true,
