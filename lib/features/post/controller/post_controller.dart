3e42e0e16ab12f4016a7b7677dfb3af58e200732
banku27 <pankajparihar123321@gmail.com>
2022-11-23 00:18:21 +0530

    share text post logic

--- lib/features/post/controller/post_controller.dart
+++ lib/features/post/controller/post_controller.dart
@@ -1,5 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/utils.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
 import 'package:reddit_clone/features/post/repository/post_repository.dart';
+import 'package:reddit_clone/models/community_model.dart';
+import 'package:reddit_clone/models/post_model.dart';
+import 'package:routemaster/routemaster.dart';
+import 'package:uuid/uuid.dart';
 
 import '../../../core/providers/storage_repository_provider.dart';
 
@@ -15,4 +22,37 @@ class PostController extends StateNotifier<bool> {
         _ref = ref,
         _storageRepository = storageRepository,
         super(false);
+
+  void shareTextPost({
+    required BuildContext context,
+    required String title,
+    required Community selectedCommunity,
+    required String description,
+  }) async {
+    state = true;
+    String postId = const Uuid().v1();
+    final user = _ref.watch(userProvider)!;
+
+    final Post post = Post(
+      id: postId,
+      title: title,
+      communityName: selectedCommunity.name,
+      communityProfilePic: selectedCommunity.avatar,
+      upvotes: [],
+      downvotes: [],
+      commentCount: 0,
+      username: user.name,
+      uid: user.uid,
+      type: 'text',
+      createdAt: DateTime.now(),
+      awards: [],
+      description: description,
+    );
+    final res = await _postRepository.addPost(post);
+    state = false;
+    res.fold((l) => showSnackBar(context, l.message), (r) {
+      showSnackBar(context, 'Posted successfully!!');
+      Routemaster.of(context).pop();
+    });
+  }
 }
