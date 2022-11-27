24cc9130eb589c31c3d41ab1236fad7d66a8c862
banku27 <pankajparihar123321@gmail.com>
2022-11-28 00:36:24 +0530

    downvotes

--- lib/features/post/repository/post_repository.dart
+++ lib/features/post/repository/post_repository.dart
@@ -76,4 +76,22 @@ class PostRepository {
       });
     }
   }
+
+  void downvote(Post post, String userId) async {
+    if (post.upvotes.contains(userId)) {
+      _posts.doc(post.id).update({
+        'upvotes': FieldValue.arrayRemove([userId]),
+      });
+    }
+
+    if (post.downvotes.contains(userId)) {
+      _posts.doc(post.id).update({
+        'downvotes': FieldValue.arrayRemove([userId]),
+      });
+    } else {
+      _posts.doc(post.id).update({
+        'downvotes': FieldValue.arrayUnion([userId]),
+      });
+    }
+  }
 }
